214065dc6b2b526c63aa3a4da48fb52861daaa17
Josh Broughton <josh.broughton47@gmail.com>
2022-12-01 15:14:28 -0700

    mongoDB atlas database set up, able to add items, had to find work around for certificate issue. working on listing plants

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import certifi
+
+ca = certifi.where()
 
 ############################################################
 # SETUP
@@ -8,9 +13,16 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
+load_dotenv()
+password = os.getenv('password')
+# was having issues with certificate validation; had to allow invalid certificates as a work around
+uri = f"mongodb+srv://admin:{password}@cluster0.rrwdoo2.mongodb.net/web-architecture?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
+app.config["MONGO_URI"] = uri
+
+
 mongo = PyMongo(app)
 
+print(mongo.db)
 ############################################################
 # ROUTES
 ############################################################
@@ -21,11 +33,12 @@ def plants_list():
 
     # TODO: Replace the following line with a database call to retrieve *all*
     # plants from the Mongo database's `plants` collection.
-    plants_data = ''
+    plants_data = mongo.db.plants.find()
 
     context = {
         'plants': plants_data,
     }
+
     return render_template('plants_list.html', **context)
 
 @app.route('/about')
@@ -37,19 +50,14 @@ def about():
 def create():
     """Display the plant creation page & process data from the creation form."""
     if request.method == 'POST':
-        # TODO: Get the new plant's name, variety, photo, & date planted, and 
-        # store them in the object below.
         new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
+            'name': request.form.get('plant_name'),
+            'variety': request.form.get('variety'),
+            'photo_url': request.form.get('photo'),
+            'date_planted': request.form.get('date_planted'),
         }
-        # TODO: Make an `insert_one` database call to insert the object into the
-        # database's `plants` collection, and get its inserted id. Pass the 
-        # inserted id into the redirect call below.
-
-        return redirect(url_for('detail', plant_id=''))
+        result = mongo.db.plants.insert_one(new_plant)
+        return redirect(url_for('detail', plant_id=result.inserted_id))
 
     else:
         return render_template('create.html')
@@ -60,7 +68,7 @@ def detail(plant_id):
 
     # TODO: Replace the following line with a database call to retrieve *one*
     # plant from the database, whose id matches the id passed in via the URL.
-    plant_to_show = ''
+    plant_to_show = mongo.db.plants.find({'_id': plant_id})
 
     # TODO: Use the `find` database operation to find all harvests for the
     # plant's id.
